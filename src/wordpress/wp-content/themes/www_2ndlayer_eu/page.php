2470540ceda4e6a7d39cb1d413f026b9fc3cd8a2
Mark Stopka <mark.stopka@perlur.cloud>
2019-05-17 16:53:46 +0200

    Refactor page.php

--- src/wordpress/wp-content/themes/www_2ndlayer_eu/page.php
+++ src/wordpress/wp-content/themes/www_2ndlayer_eu/page.php
@@ -1,23 +1,15 @@
-<?php
-/*
-*
-* Template Name: Full-Width
-*/
-?>
-
 <?php get_header(); ?>
 
     <div class="content" id="site-content">
 		<?php
-		    // Start the loop.
-		    while ( have_posts() ) : the_post();
-
-		        // Include the page content.
-		       the_content();
-
-		    // End the loop.
-		    endwhile;
-		?>
+			// Start the loop.
+			if (have_posts()) {
+				while (have_posts()) {
+					the_post();
+					// Include the page content.
+					the_content();
+				} // End the loop.
+			} ?>
 		
 	</div>
 
