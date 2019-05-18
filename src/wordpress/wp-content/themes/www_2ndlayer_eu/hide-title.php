7cc187f8a57e9fea3feb4a01f4db5218a6781e2c
Mark Stopka <mark.stopka@perlur.cloud>
2019-05-18 14:12:50 +0200

    Add hide-title.php   - Page content without title template Update page.php   - Add page title to the template

--- src/wordpress/wp-content/themes/www_2ndlayer_eu/hide-title.php
+++ src/wordpress/wp-content/themes/www_2ndlayer_eu/hide-title.php
@@ -1,3 +1,10 @@
+<?php
+/*
+*
+* Template Name: Hide Title
+*/
+?>
+
 <?php get_header(); ?>
 
     <div class="content" id="site-content">
@@ -6,8 +13,6 @@
 			if (have_posts()) {
 				while (have_posts()) {
 					the_post();
-					// Include page title.
-					the_title( '<h3>', '</h3>' );
 					// Include the page content.
 					the_content();
 				} // End the loop.
